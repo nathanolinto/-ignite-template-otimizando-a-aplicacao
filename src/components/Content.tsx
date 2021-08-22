3f06d63e309779b6e8802ab4026181a1c3820053
nathanolinto <nathanolinto@gmail.com>
2021-08-22 17:33:31 -0300

    Primeiras otimizações

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -20,6 +20,7 @@ interface ContentProps {
 }
 
 export function Content({ selectedGenre, movies }: ContentProps) {
+ 
   return (
     <div className="container">
       <header>
@@ -28,9 +29,11 @@ export function Content({ selectedGenre, movies }: ContentProps) {
 
       <main>
         <div className="movies-list">
+
           {movies.map(movie => (
             <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-          ))}
+          ))} 
+
         </div>
       </main>
     </div>
